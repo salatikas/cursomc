c8798c0456e8cdaac3974bd4466ceaf29904a97c
Rodrigo Salatiel <rodrigo.saltiel47@gmail.com>
2018-12-07 02:31:06 -0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/br/com/salatiel/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/salatiel/cursomc/services/CategoriaService.java
@@ -7,17 +7,20 @@ import org.springframework.stereotype.Service;
 
 import br.com.salatiel.cursomc.domain.Categoria;
 import br.com.salatiel.cursomc.repositories.CategoriaRepository;
+import br.com.salatiel.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
-	@Autowired //Instanciar o repositório automaticamente pelo Spring
+
+	@Autowired // Instanciar o repositório automaticamente pelo Spring
 	private CategoriaRepository repo;
-	
-	/*Esse método vai no BD, busca o Id correpondente e retorna o objeto */
+
+	/* Esse método vai no BD, busca o Id correpondente e retorna o objeto */
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! ID: " + id + ", Tipo: " + Categoria.class.getName()));
+
 	}
-	
+
 }
